d8d6b790d0945f4b976d044431e2b90baa650e05
Sabin Regmi <xawbeenregmi@gmail.com>
2021-05-15 11:21:58 +0545

    test commit
    heythere

---
+++ main.py
@@ -0,0 +1,45 @@
+from typing import Optional
+from pydantic import BaseModel
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/blog")
+def index(limit=10, published: bool = True, sort: Optional[str] = None):
+    # only get 10 published blogs
+    if (published):
+        return {'data': {f'{limit} published blogs from the database'}}
+
+    else:
+        return {'data': {f'{limit} unpublished blogs from the database'}}
+
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all unpublished blogs'}
+
+
+@app.get("/blog/{id}")
+def show(id: int):
+    # fetch blog with id = id
+
+    return {'data': id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id, limit=10):
+    # fetch comments of blog with id = id
+
+    return {'data': {'1', '2'}}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+
+@app.post('/blog')
+def create_blog(blog: Blog):
+    return {'data': f"Blog is created with {blog.title}"}
